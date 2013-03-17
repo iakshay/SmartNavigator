154a3b4f7ec559698ac7c38af7e1af2267a4b603
Akshay Aurora <akshayaurora@yahoo.com>
2013-03-17 10:09:36 +0530

    final changes

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 'use strict';
-//console.log('Hello World');
+console.log('Hello World');
 /*window.onkeydown = function(e){
-	console.log('Key Press');
+    console.log('Key Press');
 };
 
 window.addEventListener("keypress", callback, false);
@@ -17,23 +17,26 @@ var callback = function(event) {
 
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
 
-	var options = JSON.parse(localStorage.options);
+    var options = JSON.parse(localStorage.options);
     request.value = request.value.toLowerCase();
     var splitTag = request.value.split(' ')[0];
     var splitRemainder = request.value.substring(splitTag.length+1);
     var tabs = options[splitTag];
     if(tabs !== undefined){
-	    tabs.forEach(function(e, i){
-	      if(e.indexOf('{query}') >= 0){
-	      	e = e.split('{query}');
-	      	e = e[0] + splitRemainder + e[1];
-	      }
-	      chrome.tabs.create({url: e});
-	    });
-	}else{
-		console.log('foo');
-		chrome.tabs.create({url: 'http://google.com/search?q='+request.value});
-	}
+        tabs.forEach(function(e, i){
+          if(e.indexOf('{query}') >= 0){
+            e = e.split('{query}');
+            e = e[0] + splitRemainder + e[1];
+          }
+          chrome.tabs.create({url: e});
+        });
+    }else{
+        console.log('foo');
+        chrome.tabs.create({url: 'http://google.com/search?q='+request.value});
+    }
       sendResponse({farewell: "goodbye"});
  });
