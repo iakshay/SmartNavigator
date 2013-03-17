f7bb3608f009c301d38b0094c353f2639218cd22
Akshay Aurora <akshayaurora@yahoo.com>
2013-03-17 12:35:29 +0530

    finally

--- background.js
+++ background.js
@@ -13,7 +13,16 @@ var callback = function(event) {
                  alert("This doesn't work");
              }
 };*/
+var options = {
+    'social' : ['http://facebook.com', 'http://google.com/plus', 'http://twitter.com/'],
+    'shopping': ['http://www.amazon.com/s/?field-keywords={query}', 'http://www.ebay.in/sch/i.html?_trksid=p3907.m570.l1313&_nkw={query}&_sacat=0&_from=R40']
+  };
 
+  if (typeof localStorage.options == 'undefined'){
+    localStorage.options = JSON.stringify(options);
+  }else{
+    options = JSON.parse(localStorage.options);
+  }
 
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -21,10 +30,12 @@ chrome.extension.onMessage.addListener(
                 "from a content script:" + sender.tab.url :
                 "from the extension");
 
-    var options = JSON.parse(localStorage.options);
+    var options = JSON.parse(localStorage['options']);
     request.value = request.value.toLowerCase();
+
     var splitTag = request.value.split(' ')[0];
     var splitRemainder = request.value.substring(splitTag.length+1);
+
     if(!options.hasOwnProperty(splitTag)){
         console.log('foo');
         chrome.tabs.create({url: 'http://google.com/search?q='+request.value});
